5438072899d7f7a8b9ad1fe7db7d3e5114bbab26
Arjun <arjunreddy14091998@gmail.com>
2024-11-11 19:24:25 -0500

    Added folders

---
+++ theme-park-rides/build.gradle.kts
@@ -0,0 +1,12 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+    base
+}
+
+
+tasks.register<Copy>("generateDescriptions") {
+    from("descriptions")
+    into(layout.buildDirectory.dir("descriptions"))
+    filter<ReplaceTokens>("tokens" to mapOf("THEME_PARK_NAME" to "Grelephant's Wonder World"))
+}
